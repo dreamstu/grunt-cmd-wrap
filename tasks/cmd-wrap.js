6366828b90a0592f48375820eafc74052b986070
crossjs <liwenfu@crossjs.com>
2014-11-27 21:10:50 +0800

    0.1.0

--- tasks/cmd-wrap.js
+++ tasks/cmd-wrap.js
@@ -8,28 +8,13 @@
 
 module.exports = function(grunt) {
 
+  'use strict';
+
   grunt.registerMultiTask('cmd-wrap', 'run cmd-wrap with grunt.', function () {
 
-    var done = this.async();
+    this.async();
 
-    var child = grunt.util.spawn({
-        cmd: 'cmd-wrap',
-        args: [
-          '--dest', this.data.dest,
-          '--port', this.data.port,
-          '--pref', this.data.pref
-        ]
-      }, function (error, result, code) {
-        if (error) {
-          console.log(error);
-        }
-        return done(code);
-      });
-
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-
-    return child;
+    return require('../lib/wrap')(this.data);
 
   });
 
